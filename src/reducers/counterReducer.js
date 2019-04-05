9080c8c6ff226d7600f2758bb720a4aa16dbb077
maugares <mauro.garciaesteban@gmail.com>
2019-04-05 12:20:10 +0200

    repair by adding the cases to the switch functions in the reducers

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,8 @@
 export default (state = 0, action) => {
     switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+            
         default:
             return state
     }
--- src/reducers/lightswitchReducer.js
+++ src/reducers/lightswitchReducer.js
@@ -1,5 +1,8 @@
 export default (state = true, action) => {
     switch (action.type) {
+        case 'TOGGLE_LIGHTSWITCH':
+            return !state
+
         default:
             return state
     }
